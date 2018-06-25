890956905cdcbbf8c56b0d150ad005e919d4891f
Yoshiyuki Hirano <yhirano@me.com>
2018-06-25 16:46:51 +0900

    Add externals in config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
+environment.config.externals = {
+  config: JSON.stringify({
+    apiUrl: 'http://localhost:3000'
+  })
+}
 
 module.exports = environment
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+environment.config.externals = {
+  config: JSON.stringify({
+    apiUrl: ''
+  })
+}
 
 module.exports = environment.toWebpackConfig()
