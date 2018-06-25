e62cfcc77425c1a413dc542dd457b2202ead37fd
Yoshiyuki Hirano <yhirano@me.com>
2018-06-25 09:24:55 +0900

    Add App

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,18 @@
-import './hello_react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import { store } from '_helpers'
+import { App } from 'App'
+
+import { configureFakeBackend } from '_helpers'
+configureFakeBackend()
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
